e1a1ebadae75014d64f31dc4b65309d41756582d
wiltongustavo <wilton.gustavo.eu@hotmail.com>
2021-06-26 02:42:05 -0300

    implementação de notify

---
+++ .history/src/components/RoomCode_20210626021123.tsx
@@ -0,0 +1,24 @@
+import { ButtonHTMLAttributes } from 'react';
+import copyImg from '../assets/images/copy.svg';
+import '../styles/room-code.scss';
+
+type RoomCodeProps= {
+    code: string;
+    
+}
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function RoomCode(props: RoomCodeProps){
+    function copyRoomCodeToClipBoard(){
+        navigator.clipboard.writeText(props.code)
+    }
+    return(
+        <button className="room-code" onClick={copyRoomCodeToClipBoard} {...props}>
+            <div>
+                <img src={copyImg} alt="Copiar room code" />
+            </div>
+            <span>Sala #{props.code}</span>
+        </button>
+    )
+}
